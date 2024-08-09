96edbdd10311dbbf2a5144371887f841e952971c
yedbie <oey@outlook.com>
2024-08-09 16:33:40 +0300

    page iteration logic

--- script.sh
+++ script.sh
@@ -13,44 +13,50 @@ fi
 
 API_URL="https://api.github.com/user/starred"
 OUTPUT_FILE="output.md"
+PAGE=1
+PER_PAGE=100
 
-RESPONSE=$(curl -s -w "%{http_code}" -H "Authorization: token $TOKEN" $API_URL -o response.json)
+handle_error() {
+  local status_code=$1
+  if [ "$status_code" -ne 200 ]; then
+    echo "Error: Received HTTP status $status_code"
+    case "$status_code" in
+      401)
+        echo "Unauthorized: Check your API token and its permissions."
+        ;;
+      403)
+        echo "Forbidden: You might have exceeded rate limits or need different permissions."
+        ;;
+      404)
+        echo "Not Found: The URL might be incorrect."
+        ;;
+      *)
+        echo "An unknown error occurred."
+        ;;
+    esac
+    exit 1
+  fi
+}
+echo "" > $OUTPUT_FILE
 
-HTTP_STATUS="${RESPONSE: -3}"
+while true; do
+  RESPONSE=$(curl -s -w "%{http_code}" -H "Authorization: token $TOKEN" "$API_URL?page=$PAGE&per_page=$PER_PAGE" -o response.json)
+  HTTP_STATUS="${RESPONSE: -3}"
+  handle_error "$HTTP_STATUS"
+  
+  URL=$(grep -e '"url".*repos*' response.json | sed -e 's/ *"url": "https:\/\/api.github.com\/repos\/\(.*\)",/https:\/\/github.com\/\1/')
+  NAME=$(grep -e "full_name" response.json | sed -e 's/ *"full_name": "\(.*\)",/\1/')
+  DESC=$(grep -e "description" response.json | sed -e 's/ *"description": "\(.*\)",/\1/')
 
-if ! [[ "$HTTP_STATUS" =~ ^[0-9]+$ ]]; then
-  echo "Failed to retrieve HTTP status code. Exiting."
-  exit 1
-fi
-
-if [ "$HTTP_STATUS" -ne 200 ]; then
-  echo "Error: Received HTTP status $HTTP_STATUS"
-  case "$HTTP_STATUS" in
-    401)
-      echo "Unauthorized: Check your API token and its permissions."
-      ;;
-    403)
-      echo "Forbidden: You might have exceeded rate limits or need different permissions."
-      ;;
-    404)
-      echo "Not Found: The URL might be incorrect."
-      ;;
-    *)
-      echo "An unknown error occurred."
-      ;;
-  esac
-  exit 1
-fi
-
-sed '$ d' response.json > raw_data.json
-URL=$(grep -e '"url".*repos*' raw_data.json | sed -e 's/ *"url": "https:\/\/api.github.com\/repos\/\(.*\)",/https:\/\/github.com\/\1/')
-NAME=$(grep -e "full_name" raw_data.json | sed -e 's/ *"full_name": "\(.*\)",/\1/')
-DESC=$(grep -e "description" raw_data.json | sed -e 's/ *"description": "\(.*\)",/\1/')
-
-{
   paste <(echo "$NAME") <(echo "$URL") <(echo "$DESC") | while IFS=$'\t' read -r name url desc; do
     echo "* [$name]($url) $desc"
-  done
-} > $OUTPUT_FILE
+  done >> $OUTPUT_FILE
 
-echo "Data has been saved to $OUTPUT_FILE"
+  if [ $(grep -c '"url"' response.json) -lt $PER_PAGE ]; then
+    echo "Success... Data has been saved to $OUTPUT_FILE"
+    break
+  fi
+  PAGE=$((PAGE + 1))
+done
+
+rm -f response.json
